892ee43763ca454fe8345ff6d53b833a5e9ed802
sharvari257 <phatkarsharvari@gmail.com>
2021-11-01 07:40:19 +0000

    chnaged

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def hello_world():
-    return 'Hello from Gcloud Editor'
+    return 'Hello from Gcloud Editor changed'
 
 if __name__ == '_main_':
     app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
